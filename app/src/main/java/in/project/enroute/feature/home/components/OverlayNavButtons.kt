ed330a14ae200f47b4fcc93fd05f37a71c3b371e
Sidharth Dev Lal <sidharthdevlal@gmail.com>
2026-03-13 13:47:39 +0530

    unwanted import removal

--- app/src/main/java/in/project/enroute/feature/home/components/OverlayNavButtons.kt
+++ app/src/main/java/in/project/enroute/feature/home/components/OverlayNavButtons.kt
@@ -15,8 +15,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.AdminPanelSettings
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
